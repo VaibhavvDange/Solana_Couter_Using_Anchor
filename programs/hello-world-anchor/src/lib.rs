a95d5149321a87240bbb56c1ad8fd76b796342a2
VaibhavvDange <138757957+VaibhavvDange@users.noreply.github.com>
2024-03-05 11:35:25 +0530

    Update lib.rs

--- programs/hello-world-anchor/src/lib.rs
+++ programs/hello-world-anchor/src/lib.rs
@@ -1,34 +1,48 @@
 use anchor_lang::prelude::*;
 
-declare_id!("HPPStQ5VnPZr4BNJ53FYukBezuu9r5B5zMuJEJHSbKWR");
+declare_id!("4eoW5CFDVGzFGuCY3tLNCH1gkNEGMNSJ4koF74kgMZ4L");
 
 #[program]
-pub mod anchor_counter {
+pub mod anchor_boilerplate {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_user_counter(ctx: Context<CreateCounter>) -> Result<()> {
         let counter = &mut ctx.accounts.counter;
         counter.count = 0;
-        msg!("Counter account created. Current count: {}", counter.count);
+        counter.bump = ctx.bumps.counter;
+        Ok(())
+    }
+    pub fn initialize(ctx: Context<Initialize>, initial_value: u64) -> Result<()> {
+        let counter_account = &mut ctx.accounts.counter;
+        counter_account.count = initial_value;
         Ok(())
     }
 
     pub fn increment(ctx: Context<Update>) -> Result<()> {
-        let counter = &mut ctx.accounts.counter;
-        msg!("Previous counter: {}", counter.count);
-        counter.count = counter.count.checked_add(1).unwrap();
-        msg!("Counter incremented. Current count: {}", counter.count);
+        let counter_account = &mut ctx.accounts.counter;
+        counter_account.count += 1;
         Ok(())
     }
-
 
     pub fn decrement(ctx: Context<Update>) -> Result<()> {
-        let counter = &mut ctx.accounts.counter;
-        msg!("Previous counter: {}", counter.count);
-        counter.count = counter.count.checked_sub(1).unwrap();
-        msg!("Counter decremented. Current count: {}", counter.count);
+        let counter_account = &mut ctx.accounts.counter;
+        counter_account.count -= 1;
         Ok(())
     }
+}
+
+#[derive(Accounts)]
+pub struct CreateCounter<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    // space: 8 discriminator + 2 level + 4 name length + 200 name + 1 bump
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 2 + 4 + 200 + 1, seeds = [b"user-stats", user.key().as_ref()], bump
+    )]
+    pub counter: Account<'info, Counter>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
@@ -50,4 +64,5 @@ pub struct Update<'info> {
 #[account]
 pub struct Counter {
     pub count: u64,
+    pub bump: u8,
 }
